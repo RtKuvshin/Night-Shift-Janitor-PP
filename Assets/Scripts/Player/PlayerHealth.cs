e154a9295f5328a23a6f3765ee3e134543d7060b
RtKuvshin <retunskijmihail071@gmail.com>
2025-03-11 20:22:37 +0200

    Hp Pulse added

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using Microlight.MicroBar;
 
 public class PlayerHealth : MonoBehaviour
@@ -9,19 +11,88 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private int playerMaxHp = 100;
     [SerializeField] private int playerCurrentHp;
     [SerializeField] private MicroBar hpBar;
-    //[SerializeField] private int testDamage = 10;
-    //[SerializeField] private float testInterval = 3f;
+    [SerializeField] private Image lowHpImage;
+
+    private Color color;
+    private Coroutine pulseCoroutine;
 
     private void Awake()
     {
         Instance = this;
+        color = lowHpImage.color;
+        color.a = 0f; // Start fully transparent
+        lowHpImage.color = color;
     }
 
     private void Start()
     {
         playerCurrentHp = playerMaxHp;
         hpBar.Initialize(playerMaxHp);
-        //StartCoroutine(TestDamageRoutine()); 
+        StartPulsing();
+    }
+
+    private void Update()
+    {
+        if (playerCurrentHp <= 20)
+        {
+            if (pulseCoroutine == null)
+                StartPulsing();
+        }
+        else
+        {
+            if (pulseCoroutine != null)
+            {
+                StopCoroutine(pulseCoroutine); 
+                pulseCoroutine = null;
+                color.a = 0f; 
+                lowHpImage.color = color;
+            }
+        }
+    }
+
+    private void StartPulsing()
+    {
+        pulseCoroutine = StartCoroutine(PulseCoroutine());
+    }
+
+    private IEnumerator PulseCoroutine()
+    {
+        float pulseDuration = 1f; 
+        float maxAlpha = 0.5f; 
+
+        while (true)
+        {
+            
+            float pulseSpeedFactor = Mathf.Clamp01((21 - playerCurrentHp) / 20f); 
+            pulseDuration = Mathf.Lerp(1f, 0.3f, pulseSpeedFactor); 
+
+            
+            if (playerCurrentHp <= 5)
+            {
+                pulseDuration = Mathf.Max(pulseDuration, 0.5f); 
+            }
+            
+            for (float t = 0f; t < 1f; t += Time.deltaTime / pulseDuration)
+            {
+                color.a = Mathf.Lerp(0f, maxAlpha, t);
+                lowHpImage.color = color;
+                yield return null;
+            }
+
+            
+            for (float t = 0f; t < 1f; t += Time.deltaTime / pulseDuration)
+            {
+                color.a = Mathf.Lerp(maxAlpha, 0f, t);
+                lowHpImage.color = color;
+                yield return null;
+            }
+
+            
+            if (playerCurrentHp > 20)
+            {
+                break; 
+            }
+        }
     }
 
     public void ReceiveDamage(int damage)
@@ -35,14 +106,4 @@ public class PlayerHealth : MonoBehaviour
         playerCurrentHp = Mathf.Min(playerCurrentHp + hpHealed, playerMaxHp);
         hpBar.UpdateBar(playerCurrentHp);
     }
-
-   /* private IEnumerator TestDamageRoutine()
-    {
-        while (playerCurrentHp > 0)
-        {
-            yield return new WaitForSeconds(testInterval);
-            ReceiveDamage(testDamage);
-            Debug.Log($"Player took {testDamage} damage. Current HP: {playerCurrentHp}");
-        }
-    }*/ 
 }
